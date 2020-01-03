ad88aec3f36917b3942dc322373b289c7db79d5a
Jan Lippert <lippertsjan@gmail.com>
2020-01-03 21:53:56 +0100

    Added extraction of ghz to internal storage

--- sample/src/main/java/de/ironjan/arionav/sample/MainActivity.kt
+++ sample/src/main/java/de/ironjan/arionav/sample/MainActivity.kt
@@ -2,7 +2,6 @@ package de.ironjan.arionav.sample
 
 import android.Manifest.permission.*
 import android.app.Activity
-import android.content.pm.PackageManager
 import android.content.pm.PackageManager.PERMISSION_DENIED
 import android.content.pm.PackageManager.PERMISSION_GRANTED
 import androidx.appcompat.app.AppCompatActivity
@@ -13,6 +12,10 @@ import androidx.core.app.ActivityCompat.requestPermissions
 import androidx.core.app.ActivityCompat.shouldShowRequestPermissionRationale
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.*
+import java.util.zip.ZipEntry
+import java.util.zip.ZipInputStream
+
 
 class MainActivity :
     AppCompatActivity(),
@@ -26,6 +29,135 @@ class MainActivity :
         setContentView(R.layout.activity_main)
 
         requestPermissions(this)
+
+        unzipGhzToStorage(R.raw.saw, "saw-gh")
+    }
+
+    private fun unzipGhzToStorage(resId: Int, folderName: String) {
+        val targetFolderPath = "${filesDir.absolutePath}/$folderName/"
+        val targetFolder = File(targetFolderPath)
+        targetFolder.mkdirs()
+
+        // check if unzipped ghz exists
+        val timestampFileName = "_timestamp"
+
+        val timestampFile = File(targetFolderPath + timestampFileName)
+        val doesUnzipGhzExist = timestampFile.exists()
+
+        // get the timestamp of the existing unzipped ghz file
+        var timestampInExtracted: String? = null
+        if (doesUnzipGhzExist) {
+            Log.d(TAG, "Target folder for ghz file exists. Reading the existing timestamp...")
+            FileInputStream(timestampFile).use {fileInputStream ->
+                InputStreamReader(fileInputStream).use {inputStreamReader ->
+                    BufferedReader(inputStreamReader).use {bufferedReader ->
+                        timestampInExtracted = bufferedReader.readLine()
+                        Log.d(TAG, "Read timestamp from extracted file: $timestampInExtracted")
+                    }
+                }
+            }
+        }
+
+        // there is an unzipped ghz file. verify that it is the one contained in this sample.
+        var timestampInGhz: String? = ""
+        if (timestampInExtracted != null) {
+            Log.d(TAG, "Extracting timestamp from zip file.")
+            // compare with timestamp in zip
+            resources.openRawResource(resId).use { inputStream ->
+                ZipInputStream(inputStream).use { zipInputStream ->
+                    var zipEntry: ZipEntry? = zipInputStream.nextEntry
+                    while (zipEntry != null) {
+                        Log.d(TAG, "Processing ${zipEntry.name}...")
+                        if (timestampFileName == zipEntry.name) {
+                            // extract timestamp from zip and compare
+                            Log.d(TAG, "Extract timestamp for comparison.")
+                            val buffer = ByteArray(1024)
+                            val targetFile = "${targetFolder.absolutePath}/_timestamp_tmp"
+                            FileOutputStream(targetFile).use { fileOutputStream ->
+                                var count = zipInputStream.read(buffer)
+                                while (count != -1) {
+                                    Log.d(TAG, "Count: $count")
+                                    fileOutputStream.write(buffer, 0, count)
+                                    count = zipInputStream.read(buffer)
+                                }
+                            }
+                            Log.d(TAG, "Extracted timestamp entry to $targetFile")
+                            FileInputStream(targetFile).use { fileInputStream ->
+                                InputStreamReader(fileInputStream).use { inputStreamReader ->
+                                    BufferedReader(inputStreamReader).use { bufferedReader ->
+                                        timestampInGhz = bufferedReader.readLine()
+                                        Log.d(TAG, "Extracted timestamp: $timestampInGhz")
+                                    }
+                                }
+                            }
+                            File(targetFile).deleteOnExit()
+                            Log.d(TAG, "Deleting $targetFile")
+                            zipEntry = null
+                        } else {
+                            Log.d(TAG, "Skipping entry")
+                            zipInputStream.closeEntry()
+                            zipEntry = zipInputStream.nextEntry
+                        }
+
+                    }
+                }
+            }
+        }
+
+        if (timestampInGhz == timestampInExtracted) {
+            Log.d(TAG, "Timestamps do match and are both at $timestampInGhz. Skipping unzip.")
+//            return
+        }
+
+        Log.d(TAG, "Timestamps do not match: $timestampInExtracted in extracted, $timestampInGhz in ghz file. Extracting zip.")
+        resources.openRawResource(resId).use { inputStream ->
+            ZipInputStream(inputStream).use { zis ->
+                var zipEntry: ZipEntry? = zis.nextEntry
+                while (zipEntry != null) {
+                    // ghz files are flat
+                    val fileName = zipEntry?.name
+
+                    val targetFile = "$targetFolderPath$fileName"
+
+                    Log.d(TAG, "Unzipping ghz resource $folderName. Unzipping file $fileName  to $targetFile.")
+                    val buffer = ByteArray(1024)
+                    FileOutputStream(targetFile).use { fileOutputStream ->
+                        var count = zis.read(buffer)
+                        while (count != -1) {
+                            fileOutputStream.write(buffer, 0, count)
+                            count = zis.read(buffer)
+                        }
+                    }
+
+                    zis.closeEntry()
+                    zipEntry = zis.nextEntry
+                }
+                Log.d(TAG, "Completed unzip.")
+            }
+        }
+    }
+
+    private fun doActualUnzip(resId: Int, targetFolder: String, folderName: String) {
+        val inputStream = resources.openRawResource(resId)
+
+        inputStream.use {
+            val zipInputStream = ZipInputStream(inputStream)
+            zipInputStream.use {
+                var zipEntry: ZipEntry? = it.nextEntry
+                while (zipEntry != null) {
+                    // ghz files are flat
+                    val fileName = zipEntry.name
+
+                    val targetFile = "$targetFolder$fileName"
+
+                    Log.d(TAG, "Unzipping ghz resource $folderName. Unzipping file $fileName  to $targetFile.")
+                    // TODO write unzipped file
+                    //                    FileOutputStream(targetFile)
+
+                    zipEntry = it.nextEntry
+                }
+            }
+        }
     }
 
     companion object {
