54e541270389be1405178c1239321ed142c3dd94
Jan Lippert <lippertsjan@gmail.com>
2020-01-19 18:22:29 +0100

    Created new view to wrap ar sceneview

---
+++ arionav-extension/src/main/java/de/ironjan/arionav/framework/views/ArRouteView.kt
@@ -0,0 +1,12 @@
+package de.ironjan.arionav.framework.views
+
+import android.content.Context
+import android.util.AttributeSet
+
+import com.google.ar.sceneform.ArSceneView
+
+class ArRouteView : ArSceneView {
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet)
+}
