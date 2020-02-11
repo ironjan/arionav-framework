93738923979ac4351f2185fb9e702f76709bbe78
Jan Lippert <lippertsjan@gmail.com>
2020-02-11 18:17:37 +0100

    Moved class to more correct place

--- ionav/src/main/java/de/ironjan/arionav/ionav/room_routing/model/Room.kt
+++ ionav/src/main/java/de/ironjan/arionav/ionav/room_routing/model/Room.kt
@@ -1,6 +1,7 @@
-package de.ironjan.arionav.ionav.room_routing
+package de.ironjan.arionav.ionav.room_routing.model
 
 import de.ironjan.graphhopper.extensions_core.Coordinate
 
+/** Represents a simple room with doors at the given coordinates. */
 data class Room(val name: String,
                 val doors: List<Coordinate>)
