f5a33abf8b9cb92ea9ad8de34dd9a1faa85e7e81
Jan Lippert <lippertsjan@gmail.com>
2020-02-16 18:23:59 +0100

    Added basic drawer

--- sample/src/main/java/de/ironjan/arionav/sample/MainActivity.kt
+++ sample/src/main/java/de/ironjan/arionav/sample/MainActivity.kt
@@ -12,6 +12,7 @@ import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.app.ActivityCompat.requestPermissions
@@ -19,6 +20,10 @@ import androidx.core.widget.doOnTextChanged
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 import com.google.android.material.snackbar.Snackbar
 import com.graphhopper.PathWrapper
 import de.ironjan.arionav.framework.PathWrapperJsonConverter
@@ -41,20 +46,29 @@ class MainActivity :
     AppCompatActivity(),
     ActivityCompat.OnRequestPermissionsResultCallback,
     PermissionHelper.PermissionHelperCallback {
+    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
+
     val logger = LoggerFactory.getLogger("MainActivity")
     private val cameraRequestCode: Int = 1
 
     private val locationRequestCode: Int = 2
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         requestPermissions()
 
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+        actionBarDrawerToggle = ActionBarDrawerToggle(this, main_drawer_layout, R.string.drawer_open, R.string.drawer_close)
+        main_drawer_layout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
 
+        val navController = findNavController(R.id.nav_host_fragment)
+        nav_view.setupWithNavController(navController)
+
+        nav_view.setNavigationItemSelectedListener { navigateOnMenuItem(it) }
     }
 
 
@@ -65,9 +79,18 @@ class MainActivity :
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle item selection
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return navigateOnMenuItem(item)
+    }
+
+    private fun navigateOnMenuItem(item: MenuItem): Boolean {
         return when (item.itemId) {
-            R.id.mnu_feedback -> {
+            R.id.mnuFeedback -> {
                 Mailer.sendFeedback(this)
                 true
             }
@@ -77,7 +100,7 @@ class MainActivity :
 
 
     private fun requestPermissions() {
-//        PermissionHelper.requestPermission(this, CAMERA, cameraRequestCode)
+        PermissionHelper.requestPermission(this, CAMERA, cameraRequestCode)
         PermissionHelper.requestPermission(this, ACCESS_FINE_LOCATION, cameraRequestCode)
     }
 
@@ -93,7 +116,7 @@ class MainActivity :
     private fun showCameraRationale() = showPermissionRational(R.string.permission_camera_rationale, CAMERA, cameraRequestCode)
 
     private fun showPermissionRational(rationaleResId: Int, permission: String, requestCode: Int) {
-        Snackbar.make(content, rationaleResId, Snackbar.LENGTH_INDEFINITE)
+        Snackbar.make(main_drawer_layout, rationaleResId, Snackbar.LENGTH_INDEFINITE)
             .setAction(R.string.ok) {
                 requestPermissions(this, arrayOf(permission), requestCode)
             }
