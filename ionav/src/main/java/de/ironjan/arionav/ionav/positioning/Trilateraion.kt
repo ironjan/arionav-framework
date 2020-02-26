9ab3771519de5b05b871a902680fce042e5504cb
Jan Lippert <lippertsjan@gmail.com>
2020-02-26 15:51:32 +0100

    Added min level for positioning

--- ionav/src/main/java/de/ironjan/arionav/ionav/positioning/Trilateraion.kt
+++ ionav/src/main/java/de/ironjan/arionav/ionav/positioning/Trilateraion.kt
@@ -5,15 +5,20 @@ import de.ironjan.graphhopper.extensions_core.Coordinate
 import kotlin.math.round
 
 object Trilateraion {
-    fun naiveTrilateration(devices: List<SignalStrength>): Coordinate {
+    fun naiveTrilateration(devices: List<SignalStrength>, minLevel: Int = 5): Coordinate? {
         // Coordinate -> weight
         // todo probably better to not use calculateSignal from wifi manager
-        val coordToRssi = devices.map { it.coordinate to calculateSignalLevel(it.rssi, 10) / 10.0 }
+        val map = devices.map { it.coordinate to calculateSignalLevel(it.rssi, 10) / 10.0 }
+        val coordToRssi =
+            map
+                .filter { it.second >= minLevel/10.0 }
 
         val latSum = coordToRssi.map { it.first.lat * it.second }.sum()
         val lonSum = coordToRssi.map { it.first.lon * it.second }.sum()
         val lvlSum = coordToRssi.map { it.first.lvl * it.second }.sum()
-        val n = coordToRssi.map { it.second }.count().toDouble()
+        val n = coordToRssi.map { it.second }.count()
+
+        if (n==0) return null
 
         val medLat = latSum / n
         val medLon = lonSum / n
