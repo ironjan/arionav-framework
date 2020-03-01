e08e2f014371ae4db41e8b96a6171c4b43e89a56
Jan Lippert <lippertsjan@gmail.com>
2020-03-01 19:07:24 +0100

    Improved update script

--- sample/update-ghz.sh
+++ sample/update-ghz.sh
@@ -3,8 +3,17 @@ NAME="uni_paderborn"
 OSM="$MAP_FOLDER/${NAME}.osm"
 GH_FOLDER="$MAP_FOLDER/${NAME}-gh/"
 
+rm -rv ${GH_FOLDER}/*
 java -jar $HOME/projects/graphhopper/level-extension-importer/target/level-extension-importer-0.5-jar-with-dependencies.jar $OSM $GH_FOLDER
-sed -e "s/node /node version='1' /" -e "s/version='1'(.*)version='(.*)'/$1 version='$2'/" > "${GH_FOLDER}/${NAME}.osm" 
+echo "Executing SED to fix and copy osm file. May take a long time..."
+date > SED.log
+sed -E "s/node /node version='1' /" $OSM \
+ | sed -E "s/way /way version='1' /" \
+ | sed -E "s/relation /relation version='1' /" \
+ | sed -E "s/(.*)version='1'(.*)version='(.*)'(.*)/\1 \2 version='\3'\4/" \
+ | sed -E "s/.*tag(.*)v='.*version='1'.*'.*//" \
+ > "${GH_FOLDER}/${NAME}.osm" 
+date >> SED.log
 
 
 pushd $GH_FOLDER
