0f9c8c0f032a6e1fe0887c524b082364503f8e12
Jan Lippert <lippertsjan@gmail.com>
2020-04-13 01:57:21 +0200

    Better display of user position

---
+++ ionav/src/main/java/de/ironjan/arionav_fw/ionav/mapview/UserPositionLayer.java
@@ -0,0 +1,11 @@
+package de.ironjan.arionav_fw.ionav.mapview;
+
+import org.oscim.map.Map;
+
+public class UserPositionLayer extends org.oscim.layers.LocationLayer {
+
+    public UserPositionLayer(Map map) {
+        super(map, 1);
+    }
+
+}
