9844e49bac1303b0b2e2e0af7e1f2af392bdbc69
Jan Lippert <lippertsjan@gmail.com>
2020-04-21 17:33:08 +0200

    removed unused code

--- arionav-extension/src/main/java/de/ironjan/arionav_fw/framework/arionav/SimplifiedRoute.kt
+++
@@ -1,9 +0,0 @@
-package de.ironjan.arionav_fw.framework.arionav
-
-import com.graphhopper.util.Instruction
-import com.graphhopper.util.PointList
-
-data class SimplifiedRoute(val waypoints: PointList,
-                           val instructionList: List<Instruction>){
-  val zipped = waypoints.zip(instructionList)
-}
