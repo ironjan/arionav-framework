154364d46818de425c83b6bc6ed2fb41892ef290
Jan Lippert <lippertsjan@gmail.com>
2020-04-29 15:52:54 +0200

    Removed comment

--- ionav/src/main/java/de/ironjan/arionav_fw/ionav/positioning/IPositionObserver.kt
+++ ionav/src/main/java/de/ironjan/arionav_fw/ionav/positioning/IPositionObserver.kt
@@ -3,5 +3,4 @@ package de.ironjan.arionav_fw.ionav.positioning
 import de.ironjan.arionav_fw.ionav.util.Observer
 
 interface IPositionObserver : Observer<IonavLocation?> {
-//  override fun update(c: IonavLocation?)
 }
