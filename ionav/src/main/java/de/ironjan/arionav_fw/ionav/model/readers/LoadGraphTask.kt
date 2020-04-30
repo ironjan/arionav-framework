f57c678e8d2d77ffb6f1e59e4578aba884ac9983
Jan Lippert <lippertsjan@gmail.com>
2020-04-30 04:34:56 +0200

    simplified LoadGraphTask.kt

--- ionav/src/main/java/de/ironjan/arionav_fw/ionav/model/readers/LoadGraphTask.kt
+++ ionav/src/main/java/de/ironjan/arionav_fw/ionav/model/readers/LoadGraphTask.kt
@@ -1,38 +1,28 @@
 package de.ironjan.arionav_fw.ionav.model.readers
 
 import android.os.AsyncTask
+import arrow.core.Either
+import arrow.core.Left
+import arrow.core.Right
 import com.graphhopper.GraphHopper
 import de.ironjan.graphhopper.levelextension.GraphLoader
 
-class LoadGraphTask(private val mapsFolder: String, private val callback: Callback) : AsyncTask<Void, Void, GraphHopper?>() {
-    private var hopper: GraphHopper?  = null
-    private var exception: Exception? = null
+class LoadGraphTask(private val mapsFolder: String, private val callback: Callback) : AsyncTask<Void, Void, Either<Exception, GraphHopper>>() {
 
-    override fun doInBackground(vararg p0: Void?): GraphHopper? {
+    override fun doInBackground(vararg p0: Void?): Either<Exception, GraphHopper> =
         try {
-            val tmpHopper = GraphLoader.loadExisting(mapsFolder)
-            hopper = tmpHopper
-            return hopper
+            Right(GraphLoader.loadExisting(mapsFolder))
+        } catch (e: Exception) {
+            Left(e)
         }
-        catch (e: Exception) {
-            exception = e
-        }
-        return null
-    }
 
-    override fun onPostExecute(result: GraphHopper?) {
+    override fun onPostExecute(result: Either<Exception, GraphHopper>) {
         super.onPostExecute(result)
-        val lException = exception
-        if(lException != null) {
-            callback.onError(lException)
-        } else {
-            callback.onSuccess(hopper!!)
+
+        when (result) {
+            is Either.Left -> callback.onError(result.a)
+            is Either.Right -> callback.onSuccess(result.b)
         }
-
-    }
-
-    companion object {
-        const val TAG = "Foo"
     }
 
     interface Callback {
