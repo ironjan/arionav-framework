5c49d40c7f5bbf07bbae0b4f1925df65ff51b898
Jan Lippert <lippertsjan@gmail.com>
2020-06-02 14:26:39 +0200

    Cleaned up IndoorDestinationService.kt

--- ionav/src/main/java/de/ironjan/arionav_fw/ionav/services/IndoorDestinationService.kt
+++ ionav/src/main/java/de/ironjan/arionav_fw/ionav/services/IndoorDestinationService.kt
@@ -1,19 +1,8 @@
 package de.ironjan.arionav_fw.ionav.services
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import de.ironjan.arionav_fw.ionav.model.indoor_map.IndoorData
 import de.ironjan.graphhopper.extensions_core.Coordinate
 
 class IndoorDestinationService(private val indoorDataService: IndoorDataService) {
-
-    private val _indoorData = MutableLiveData<IndoorData>(IndoorData.empty())
-    val indoorData: LiveData<IndoorData> = _indoorData
-
-    private val _isIndoorDataLoaded: Boolean
-        get() = _loadingState == IndoorDataLoadingState.READY
-
-    private var _loadingState = IndoorDataLoadingState.INITIALIZED
 
     /**
      * Tries to convert {@code value} into a {@code Coordinate}.
