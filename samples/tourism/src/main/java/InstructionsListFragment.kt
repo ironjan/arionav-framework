4600cb153d5468ba526f6116a73144cbc44a57c3
Jan Lippert <lippertsjan@gmail.com>
2020-06-29 15:16:20 +0200

    Removed unused class

--- samples/tourism/src/main/java/InstructionsListFragment.kt
+++
@@ -1,7 +0,0 @@
-import androidx.fragment.app.activityViewModels
-import de.ironjan.arionav_fw.ionav.views.NavigationViaInstructionsFragment
-import de.ironjan.arionav_fw.samples.tourism.viewmodel.TourismViewModel
-
-class InstructionsListFragment: NavigationViaInstructionsFragment() {
-    override val viewModel: TourismViewModel by activityViewModels()
-}
